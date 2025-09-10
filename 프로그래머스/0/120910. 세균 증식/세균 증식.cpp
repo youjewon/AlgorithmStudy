96535019d1c06462d5ac502fc5e3b1f9972ec059
WHXHW <35909740+youjewon@users.noreply.github.com>
2025-09-10 10:11:45 +0900

    [level 0] Title: 세균 증식, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int t) {
+    int answer = 0;
+    for(int i = 0;i < t;i++)
+    {
+         n = n * 2;
+    }
+    
+    return n;
+}
