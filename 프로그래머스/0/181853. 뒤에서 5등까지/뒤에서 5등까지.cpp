b34cfc81a02a601b855e52d8706065f182fa3c87
jemni <35909740+youjewon@users.noreply.github.com>
2025-10-14 17:55:43 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer;
+    sort(num_list.begin(),num_list.end());
+    answer.assign(num_list.begin(),num_list.begin()+5);
+    return answer;
+}
