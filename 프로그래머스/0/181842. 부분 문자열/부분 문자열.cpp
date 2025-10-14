2233cad35d7139c10c45e16377451618f7d547f4
jemni <35909740+youjewon@users.noreply.github.com>
2025-10-14 16:38:00 +0900

    [level 0] Title: 부분 문자열, Time: 0.02 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string str1, string str2) {
+    int answer = 0;
+    
+    return str2.find(str1) != string::npos ? 1 : 0;
+}
