e6693268abe09e057f60a1f06d1fcbd36418c907
jemni <35909740+youjewon@users.noreply.github.com>
2025-10-15 12:58:57 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.10 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+vector<string> solution(string my_string) {
+    vector<string> answer;
+    
+    istringstream iss(my_string);
+    string word;
+    
+    while(iss >> word) {
+        answer.push_back(word);
+    }
+    return answer;
+}
